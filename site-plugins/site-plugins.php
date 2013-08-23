74fc2f2da96057f59f1709aa3fa142a3b668e5af
Martin Szymanski <martin@elfacht.com>
2013-08-23 09:51:07 +0200

    added function to disable plugin deactivation

--- site-plugins/site-plugins.php
+++ site-plugins/site-plugins.php
@@ -67,6 +67,22 @@ function e8_widgets_init() {
 add_action('widgets_init', 'e8_widgets_init');
 
 
+/* =Disable plugin deactivation / removes the deactivation link
+------------------------------------------------------*/
+add_filter('plugin_action_links', 'e8_lock_plugins', 10, 4);
+function e8_lock_plugins($actions, $plugin_file, $plugin_data, $context) {
+  // Remove edit link for all
+  if (array_key_exists('edit', $actions))
+      unset($actions['edit']);
+  // Remove deactivate link for crucial plugins
+  if (array_key_exists('deactivate', $actions) && in_array($plugin_file, array(
+      'plugin-name/plugin-name.php'
+  )))
+      unset($actions['deactivate']);
+  return $actions;
+}
+
+
 /* =Adds RSS link in <head>
 ------------------------------------------------------*/
 add_theme_support('automatic-feed-links');
